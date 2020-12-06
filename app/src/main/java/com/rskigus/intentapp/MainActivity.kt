6a4f477761c1105e5992fe9dbf5b72cc5f6275c8
rskigus10 <71111285+rskigus10@users.noreply.github.com>
2020-12-06 23:08:00 +0700

    intent

--- app/src/main/java/com/rskigus/intentapp/MainActivity.kt
+++ app/src/main/java/com/rskigus/intentapp/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.rskigus.intentapp
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import com.rskigus.intentapp.model.User
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val REQUEST_CODE = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,6 +44,20 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:$phoneNumber"))
             intent.putExtra("sms_body", "Hayukkkkk cari tau")
             startActivity(intent)
+        }
+
+        btn_intent_result.setOnClickListener {
+            val intent = Intent(this@MainActivity, ResultActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == 200){
+            val color = data?.getStringExtra(ResultActivity.EXTRA_COLOR)
+            Log.d("Color", color.toString())
+            view_result.setBackgroundColor(Color.parseColor(color.toString()))
         }
     }
 }
